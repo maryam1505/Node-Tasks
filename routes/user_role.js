313b3c4a2cf8867ddb0ce1499a94e7535e052295
Ayesha Maryam <ayesham.fos1505@gmail.com>
2024-09-04 13:43:14 +0500

    Role updated

--- routes/user_role.js
+++ routes/user_role.js
@@ -3,6 +3,29 @@ import db from "../db.js";
 
 const router = express.Router();
 
+router.get("/fetch_UserRole/:id", (req, res) => {
+  const userId = req.params.id;
+
+  if (!userId) {
+    return res.status(400).json({ error: "userId is required" });
+  }
+
+  const sql = `
+    SELECT roles.id, roles.title
+    FROM user_roles
+    JOIN roles ON user_roles.role_id = roles.id
+    WHERE user_roles.user_id = ?
+    `;
+    db.query(sql, [userId], (err, results) => {
+      if (err) {
+        return res.status(500).json({ error: err.message });
+      }
+      if (results.length === 0) {
+        return res.status(404).json({ message: "No roles found for this user" });
+      }
+      res.status(200).json({ results });
+    });
+});
 router.post("/assign_UserRole", (req, res) => {
   const { userId, roleId } = req.body;
 
@@ -35,7 +58,7 @@ router.delete("/delete_UserRole", (req, res) => {
   });
 });
 
-router.get('/get_UserRoles/:userId', (req, res) => {
+router.get("/get_UserRoles/:userId", (req, res) => {
   const userId = req.params.userId;
 
   const sql = `
